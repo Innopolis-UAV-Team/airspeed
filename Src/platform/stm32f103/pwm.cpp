805080999170df0fb39a6bcdc30d2798dac25c5d
AsiiaPine <asiiapine@gmail.com>
2024-02-01 00:35:23 +0300

    fix for stm

--- Src/platform/stm32f103/pwm.cpp
+++ Src/platform/stm32f103/pwm.cpp
@@ -5,8 +5,10 @@
 #include "periphery/pwm/pwm.hpp"
 #include "main.h"
 
-extern TIM_HandleTypeDef htim1;
+#ifdef htim2
 extern TIM_HandleTypeDef htim2;
+#endif
+extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim3;
 
 // PWM_1,      // PA8 TIM1_CH1 INT_LED_GREEN
@@ -16,48 +18,6 @@ extern TIM_HandleTypeDef htim3;
 // PWM_5,      // PB5 TIM3_CH2
     
 Logger PwmPeriphery::_logger;
-// int8_t PwmPeriphery::init(PwmPin pwm_pin) {
-//     _logger.init("PWM");
-//     switch (pwm_pin) {
-//         case PwmPin::PWM_1:
-//             if (HAL_OK != HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1)) {
-//                 return -1;
-//             }
-//             TIM1->CCR2 = 1000;
-//             break;
-
-//         case PwmPin::PWM_2:
-//             if (HAL_OK != HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1)) {
-//                 return -1;
-//             }
-//             TIM2->CCR1 = 1000;
-//             break;
-
-//         case PwmPin::PWM_3:
-//             if (HAL_OK != HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3)) {
-//                 return -1;
-//             }
-//             TIM3->CCR3 = 1000;
-//             break;
-
-//         case PwmPin::PWM_4:
-//             if (HAL_OK != HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1)) {
-//                 return -1;
-//             }
-//             TIM3->CCR1 = 1000;
-//             break;
-//         case PwmPin::PWM_5:
-//             if (HAL_OK != HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2)) {
-//                 return -1;
-//             }
-//             TIM3->CCR2 = 1000;
-//             break;
-//         default:
-//             return -1;
-//     }
-
-//     return 0;
-// }
 
 void PwmPeriphery::set_duration(const PwmPin pwm_pin, uint32_t duration_us) {
     switch (pwm_pin) {
@@ -66,7 +26,9 @@ void PwmPeriphery::set_duration(const PwmPin pwm_pin, uint32_t duration_us) {
             break;
 
         case PwmPin::PWM_2:
+            #ifdef htim2
             TIM2->CCR1 = duration_us;
+            #endif
             break;
 
         case PwmPin::PWM_3:
@@ -94,7 +56,9 @@ uint32_t PwmPeriphery::get_duration(PwmPin pwm_pin) {
             break;
 
         case PwmPin::PWM_2:
+            #ifdef htim2
             pwm_duration = TIM2->CCR1;
+            #endif
             break;
 
         case PwmPin::PWM_3:
@@ -133,7 +97,9 @@ void PwmPeriphery::set_duty_cycle(PwmPin pwm_pin, uint8_t duty_cycle_pct){
 
         case PwmPin::PWM_2:
             // _HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, (int) fraction * TIM4->ARR - 1);
+            #ifdef htim2
             TIM2->CCR1 = (int) fraction * TIM2->ARR - 1;
+            #endif
             break;
 
         case PwmPin::PWM_3:
@@ -161,39 +127,31 @@ void PwmPeriphery::reset(PwmPin pwm_pin){
 
     switch (pwm_pin) {
         case PwmPin::PWM_1:
-            // _HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (int) fraction * TIM1->ARR - 1);
             HAL_TIM_PWM_Stop_IT(&htim1, TIM_CHANNEL_1);
             // __HAL_TIM_DISABLE_IT(htim1, TIM_IT_CC3);
 
             break;
-
+        
         case PwmPin::PWM_2:
+            #ifdef htim2
             HAL_TIM_PWM_Stop_IT(&htim2, TIM_CHANNEL_1);
-            
-            // _HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, (int) fraction * TIM4->ARR - 1);
-            // TIM4->CCR1 = (int) fraction * TIM4->ARR - 1;
+            #endif
             break;
 
         case PwmPin::PWM_3:
             HAL_TIM_PWM_Stop_IT(&htim3, TIM_CHANNEL_3);
 
-            // _HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, (int) fraction * TIM3->ARR - 1);
-            // TIM3->CCR3 = (int) fraction * TIM3->ARR - 1;
 
             break;
 
         case PwmPin::PWM_4:
             HAL_TIM_PWM_Stop_IT(&htim3, TIM_CHANNEL_1);
 
-            // _HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, (int) fraction * TIM3->ARR - 1);
-            // TIM3->CCR1 = (int) fraction * TIM3->ARR - 1;
             break;
 
         case PwmPin::PWM_5:
-            // _HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, (int) fraction * TIM3->ARR - 1);
             HAL_TIM_PWM_Stop_IT(&htim3, TIM_CHANNEL_2);
             
-            // TIM3->CCR2 = (int) fraction * TIM3->ARR - 1;
             break;
         default:
             _logger.log_debug("No such PIN");
