ed1ca6d4374de1f590314b32945ed33d941e84c3
AsiiaPine <asiiapine@gmail.com>
2024-03-04 15:34:55 +0300

    add light verbose mode

--- Src/platform/ubuntu/pwm.cpp
+++ Src/platform/ubuntu/pwm.cpp
@@ -4,22 +4,32 @@
 
 #include "periphery/pwm/pwm.hpp"
 #include <cstddef>
+uint16_t pwm[(int)PwmPin::PWM_AMOUNT];
 
 void PwmPeriphery::init(PwmPin){}
 
 void PwmPeriphery::set_duration(const PwmPin pwm_pin, uint16_t duration_us) {
     (void)pwm_pin;
     (void)duration_us;
+    pwm[(int) pwm_pin] = duration_us;
 }
 
 uint16_t PwmPeriphery::get_duration(PwmPin pwm_pin) {
     (void)pwm_pin;
-    return 0;
+    return pwm[(int)pwm_pin];
 }
 void PwmPeriphery::set_duty_cycle_pct(PwmPin pwm_pin, uint8_t duty_cycle_pct){
     (void)pwm_pin;
+    if (duty_cycle_pct > 100){
+        return;
+    }
+
+    float fraction = 1.0 - (duty_cycle_pct / 100.0);
+
+    pwm[(int)pwm_pin] = (fraction * 1000);
 }
 void PwmPeriphery::reset(PwmPin pwm_pin){
     (void)pwm_pin;
+    pwm[(int) pwm_pin] = 1000;
 }
 
