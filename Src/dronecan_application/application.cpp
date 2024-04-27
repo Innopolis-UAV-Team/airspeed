c802b7c2c9c856b5e9b7cbe078c99bba0d01dbd8
Anastasiia Stepanova <asiiapine@gmail.com>
2024-04-27 13:14:09 +0300

    upd modules (#9)

--- Src/dronecan_application/application.cpp
+++ Src/dronecan_application/application.cpp
@@ -1,46 +1,47 @@
 /***
  * Copyright (C) 2024 Anastasiia Stepanova  <asiiapine96@gmail.com>
- *  Distributed under the terms of the GPL v3 license, available in the file LICENSE.
-***/ 
+ *  Distributed under the terms of the GPL v3 license, available in the file
+ *LICENSE.
+ ***/
 
 #include "application.hpp"
 #include "dronecan.h"
-#include "main.h"
-#include "params.hpp"
 #include "logger.hpp"
-#include "modules/LightsModule.hpp"
+#include "main.h"
 #include "modules/CircuitStatusModule.hpp"
+#include "modules/LightsModule.hpp"
+#include "params.hpp"
 
 #ifdef HAL_IWDG_MODULE_ENABLED
 extern IWDG_HandleTypeDef hiwdg;
 #endif /* HAL_IWDG_MODULE_ENABLED */
 
 void application_entry_point() {
+    paramsInit(static_cast<uint8_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT),
+               NUM_OF_STR_PARAMS, -1, 1);
+    paramsLoad();
 
-    paramsInit(static_cast<uint8_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT), NUM_OF_STR_PARAMS);
-    paramsLoadFromFlash();
-
-    auto node_id = paramsGetIntegerValue(IntParamsIndexes::PARAM_UAVCAN_NODE_ID);
+    auto node_id =
+        paramsGetIntegerValue(IntParamsIndexes::PARAM_UAVCAN_NODE_ID);
 
     const auto node_name = "arl.lights";
     Logger logger = Logger(node_name);
-    auto node_name_param_idx = static_cast<ParamIndex_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT);
+    auto node_name_param_idx =
+        static_cast<ParamIndex_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT);
 
     paramsSetStringValue(node_name_param_idx, 19, (const uint8_t*)node_name);
     uavcanSetNodeName(node_name);
 
     uavcanInitApplication(node_id);
-    
     LightsModule& light_module = LightsModule::get_instance();
     CircuitStatusModule& status_module = CircuitStatusModule::get_instance();
     while (true) {
         light_module.spin_once();
         status_module.spin_once();
-
         uavcanSpinOnce();
 
-        #ifdef HAL_IWDG_MODULE_ENABLED
+#ifdef HAL_IWDG_MODULE_ENABLED
         HAL_IWDG_Refresh(&hiwdg);
-        #endif  // HAL_IWDG_MODULE_ENABLED
+#endif  // HAL_IWDG_MODULE_ENABLED
     }
 }
