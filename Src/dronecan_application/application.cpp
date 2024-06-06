96a9f1a728200b3e86df67100d99b57ae383f56f
AsiiaPine <asiiapine@gmail.com>
2024-06-06 17:21:17 +0300

    formWrite

--- Src/dronecan_application/application.cpp
+++ Src/dronecan_application/application.cpp
@@ -22,6 +22,8 @@ extern IWDG_HandleTypeDef hiwdg;
 void application_entry_point() {
     paramsInit(static_cast<uint8_t>(IntParamsIndexes::INTEGER_PARAMS_AMOUNT),
                NUM_OF_STR_PARAMS, -1, 1);
+    // paramsInitRedundantPage(126);
+    // paramsChooseRom();
     paramsLoad();
 
     auto node_id =
